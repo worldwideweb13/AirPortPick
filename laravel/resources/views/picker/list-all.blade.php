4d9929abdd85080fe4d50c0dbcf488f4170bf0b5
yanagi <u.conf.324@gmail.com>
2020-12-18 15:34:54 +0900

    create-cssfile/change-page
    共通ページを修正、個別のCSSを作成、それぞれのページで読み込ませる処理を作った。

--- laravel/resources/views/picker/list-all.blade.php
+++ laravel/resources/views/picker/list-all.blade.php
@@ -1,5 +1,9 @@
 @extends('picker.layouts.common')
 
+@push('css')
+<link rel="stylesheet" href="{{ asset('/css/picker/list-all.css') }}">
+@endpush
+
 @section('title',"ホーム")
 
 @section('header_title',"割り当てリスト")
