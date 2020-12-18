9297d0c103112bc745aaa7f2c2764bb81cfd7163
yanagi <u.conf.324@gmail.com>
2020-12-18 19:18:20 +0900

    cerate-oderdetailpage

---
+++ laravel/resources/views/picker/oder-details.blade.php
@@ -0,0 +1,13 @@
+@extends('picker.layouts.common')
+
+@push('css')
+<link rel="stylesheet" href="{{ asset('/css/picker/oder-details.css') }}">
+@endpush
+
+@section('title',"注文詳細")
+
+@section('header_title',"注文番号：000")
+
+@section('content')
+
+@endsection
