21b13848040151431739e307da1cc1d41e915ed8
yanagi <u.conf.324@gmail.com>
2020-12-19 17:18:09 +0900

    edit-pickerpage
    ピッカーの管理画面のレイアウトを変種しました。

--- laravel/resources/views/picker/oder-details.blade.php
+++ laravel/resources/views/picker/oder-details.blade.php
@@ -10,54 +10,67 @@
 
 @section('content')
 <div class="oder-details">
-  <div class="card-deck">
+
+  <div class="card-wrapper">
 
     <div class="card">
-      <img src="{{asset('/images/white.jpeg')}}" class="card-img-top" width="100%" height="180">
+      <img src="{{asset('/images/white.jpeg')}}" class="card-img-top" width="100" height="180">
       <div class="card-body">
-        <p class="card-title">Card title</p>
-        <h5 class="card-text">商品説明がここに入る。商品説明がここに入る。商品説明がここに入る。</h5>
+        <p class="card-title">白い恋人</p>
+        <h5 class="card-text">保存：常温<br>金額：500円<br>店舗：ショップ小笠原</h5>
       </div>
       <div class="card-footer">
-        <small class="text-muted">Last updated 3 mins ago</small>
+        <small class="text-muted">
+          <label><input type="radio" name="q1" value="true"> 購入済み</label>
+          <label><input type="radio" name="q1" value="false"> 売り切れ</label>
+        </small>
       </div>
     </div>
 
     <div class="card">
-      <img src="{{asset('/images/white.jpeg')}}" class="card-img-top" width="100%" height="180">
+      <img src="{{asset('/images/white.jpeg')}}" class="card-img-top" width="100" height="180">
       <div class="card-body">
-        <p class="card-title">Card title</p>
-        <h5 class="card-text">商品説明がここに入る。商品説明がここに入る。商品説明がここに入る。</h5>
+        <p class="card-title">白い恋人</p>
+        <h5 class="card-text">常温<br>金額：500円<br>店舗：ショップ小笠原</h5>
       </div>
       <div class="card-footer">
-        <small class="text-muted">Last updated 3 mins ago</small>
+        <small class="text-muted">
+          <label><input type="radio" name="q1" value="true"> 購入済み</label>
+          <label><input type="radio" name="q1" value="false"> 売り切れ</label>
+        </small>
       </div>
     </div>
 
     <div class="card">
-      <img src="{{asset('/images/white.jpeg')}}" class="card-img-top" width="100%" height="180">
+      <img src="{{asset('/images/white.jpeg')}}" class="card-img-top" width="100" height="180">
       <div class="card-body">
-        <p class="card-title">Card title</p>
-        <h5 class="card-text">商品説明がここに入る。商品説明がここに入る。商品説明がここに入る。</h5>
-        <h5 class="card-text">冷凍</h5>
-        <h5 class="card-text">¥2000</h5>
+        <p class="card-title">白い恋人</p>
+        <h5 class="card-text">常温<br>金額：500円<br>店舗：ショップ小笠原</h5>
       </div>
       <div class="card-footer">
-        <small class="text-muted">Last updated 3 mins ago</small>
+        <small class="text-muted">
+          <label><input type="radio" name="q1" value="true"> 購入済み</label>
+          <label><input type="radio" name="q1" value="false"> 売り切れ</label>
+        </small>
       </div>
     </div>
 
     <div class="card">
-      <img src="{{asset('/images/white.jpeg')}}" class="card-img-top" width="100%" height="180">
+      <img src="{{asset('/images/white.jpeg')}}" class="card-img-top" width="100" height="180">
       <div class="card-body">
-        <p class="card-title">Card title</p>
-        <h5 class="card-text">商品説明がここに入る。商品説明がここに入る。商品説明がここに入る。</h5>
+        <p class="card-title">白い恋人</p>
+        <h5 class="card-text">常温<br>金額：500円<br>店舗：ショップ小笠原</h5>
       </div>
       <div class="card-footer">
-        <small class="text-muted">Last updated 3 mins ago</small>
+        <small class="text-muted">
+          <label><input type="radio" name="q1" value="true"> 購入済み</label>
+          <label><input type="radio" name="q1" value="false"> 売り切れ</label>
+        </small>
       </div>
     </div>
-
   </div>
+
+  <center><button class="btn-lg btn-primary">ピックアップ完了</button></center>
+
 </div>
 @endsection
