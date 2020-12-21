76559ba74672d80247d93f98c62724466d5f2084
yanagi <u.conf.324@gmail.com>
2020-12-21 20:16:24 +0900

    create-js

--- laravel/public/js/manage/index.js
+++ laravel/public/js/manage/index.js
@@ -1,19 +1,16 @@
 // bootstrap プルダウンメニュー動作
-$(function(){
-    $('.dropdown-menu .dropdown-item').click(function(){
+$(function () {
+    $('.dropdown-menu .dropdown-item').click(function () {
         var visibleItem = $('.dropdown-toggle', $(this).closest('.dropdown'));
         visibleItem.text($(this).attr('value'));
     });
 });
-
 // モーダルコンテンツ表示
-$('.js-modal-open').on('click',function(){
+$('.js-modal-open').on('click', function () {
     $('.js-modal').fadeIn();
     return false;
 });
-$('.js-modal-close').on('click',function(){
+$('.js-modal-close').on('click', function () {
     $('.js-modal').fadeOut();
     return false;
 });
-
-
--- laravel/public/js/picker/click.js
+++ laravel/public/js/picker/click.js
@@ -9,7 +9,7 @@ jQuery(function ($) {
       //e.targetはクリックした要素自体、それがa要素以外であれば
       if (!$(e.target).is('a')) {
 
-        //その要素の先祖要素で一番近いdivの
+        //その要素の先祖要素で一番近いtrの
         //data-href属性の値に書かれているURLに遷移する
         window.location = $(e.target).closest('div').data('href');
       }
